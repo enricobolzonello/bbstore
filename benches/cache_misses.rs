bd9a857f5cf313f34e0947132a8aab387b9f4dd6
Enrico Bolzonello <enribolzonello@gmail.com>
2026-04-15 18:20:57 +0200

    feat: add benchmarks for cache misses and example

---
+++ benches/cache_misses.rs
@@ -0,0 +1,43 @@
+use bbstore::BBStore;
+use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
+use std::sync::Arc;
+
+fn bench_working_set_size(c: &mut Criterion) {
+    let mut group = c.benchmark_group("WorkingSetSize");
+
+    for size in [1_000, 10_000, 100_000, 1_000_000].iter() {
+        let num_shards = 4;
+        let store = Arc::new(BBStore::new(num_shards));
+
+        for i in 0..*size {
+            let _ = store.insert(format!("key-{}", i), "value".to_string());
+        }
+
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &s| {
+            b.iter(|| {
+                // Random access to trigger cache misses
+                let key = format!("key-{}", rand::random::<usize>() % s);
+                let _ = black_box(store.get(key));
+            });
+        });
+    }
+    group.finish();
+}
+
+fn bench_batch_impact(c: &mut Criterion) {
+    let mut group = c.benchmark_group("BatchingImpact");
+    let store = Arc::new(BBStore::new(1)); // Single shard to force high load
+
+    // We flood the actor to ensure the batching logic (try_recv) triggers
+    group.bench_function("flood_sequential", |b| {
+        b.iter(|| {
+            for i in 0..64 {
+                let _ = black_box(store.get(format!("key-{}", i)));
+            }
+        });
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench_working_set_size, bench_batch_impact);
+criterion_main!(benches);
---
+++ examples/basic.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use bbstore::BBStore;
+use std::sync::Arc;
+
+fn main() -> Result<()> {
+    let store = Arc::new(BBStore::new(1));
+    store.insert("key-1".into(), "value-1".into())?;
+    store.insert("key-2".into(), "value-2".into())?;
+    store.insert("key-3".into(), "value-3".into())?;
+    store.insert("key-4".into(), "value-4".into())?;
+    store.insert("key-5".into(), "value-5".into())?;
+    store.insert("key-6".into(), "value-6".into())?;
+    store.insert("key-7".into(), "value-7".into())?;
+    store.insert("key-8".into(), "value-8".into())?;
+
+    std::thread::scope(|s| {
+        for i in 1..9 {
+            let thread_store = Arc::clone(&store);
+            s.spawn(move || {
+                let key = format!("key-{}", i);
+                println!("{:?}", thread_store.get(key));
+            });
+        }
+    });
+
+    Ok(())
+}
