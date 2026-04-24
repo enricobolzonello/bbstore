376d55f6ed6c09d83f05847b48ca72d2cbc6cda4
Enrico Bolzonello <enribolzonello@gmail.com>
2026-04-24 15:11:48 +0200

    refactor(cli): use subcommands instead of args

--- src/bin/bbcli.rs
+++ src/bin/bbcli.rs
@@ -1,43 +1,31 @@
 use anyhow::Result;
-use clap::{Parser, ValueEnum};
-use std::fmt;
+use clap::{Parser, Subcommand};
 
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 
-#[derive(Clone, ValueEnum, Debug)]
+#[derive(Subcommand, Clone, Debug)]
 enum InputCommands {
-    Set,
-    Get,
-}
-
-impl fmt::Display for InputCommands {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Get => write!(f, "GET"),
-            Self::Set => write!(f, "SET"),
-        }
-    }
+    #[command(name = "SET")]
+    Set { key: String, value: String },
+    #[command(name = "GET")]
+    Get { key: String },
 }
 
 #[derive(Parser)]
 struct Args {
-    #[arg(required = true, value_enum, ignore_case = true)]
+    #[clap(subcommand)]
     command: InputCommands,
-
-    #[arg(required = true)]
-    key: String,
-
-    #[arg(required_if_eq("command", "SET"))]
-    value: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
     let mut writer = TcpStream::connect("127.0.0.1:8080").await?; // TODO: global config file to set the same ip address as the store
-    let value_part = args.value.map(|v| format!(" {}", v)).unwrap_or_default();
-    let command = format!("{} {}{}\n", args.command, args.key, value_part);
+    let command = match &args.command {
+        InputCommands::Set { key, value } => format!("SET {} {}\n", key, value),
+        InputCommands::Get { key } => format!("GET {}\n", key),
+    };
     writer.write_all(command.as_bytes()).await?;
 
     let mut response = String::new();
